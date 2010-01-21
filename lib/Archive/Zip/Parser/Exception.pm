6e06facca472916b711e3ff1abe01df2f4ce26e1
Alan Haggai Alavi <alanhaggai@alanhaggai.org>
2010-01-21 17:30:03 +0530

    No need to create an object

--- lib/Archive/Zip/Parser/Exception.pm
+++ lib/Archive/Zip/Parser/Exception.pm
@@ -4,13 +4,6 @@ use warnings;
 use strict;
 use Carp;
 
-sub new {
-    my $class = shift;
-
-    my $new_object = bless {}, $class;
-    return $new_object;
-}
-
 sub _croak {
     my ( $self, $error_message ) = @_;
     my $caller_package = (caller)[0];
